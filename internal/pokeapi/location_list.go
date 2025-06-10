22035b6472d11d428b2f5ca6ca37918fb6aedd3c
Fabio Dantas Fernandes <nunseik@gmail.com>
2025-06-09 23:28:21 -0300

    internal basic logic

---
+++ internal/pokeapi/location_list.go
@@ -0,0 +1,12 @@
+package pokeapi
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+)
+
+// ListLocations -
+func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
+	return RespShallowLocations{}, nil
+}
